2e4b83271d2d55ce3e7c019469059602f35a1d51
Elisaveta Zobeva <e.zobeva@gmail.com>
2022-06-08 12:10:38 +0100

    register testing  invalid credentials

--- server.test.js
+++ server.test.js
@@ -1,10 +1,17 @@
 const app = require("./server");
 const supertest = require("supertest");
 const request = supertest(app);
-import test from "node:test";
 
-it("gets the test endpoint", async () => {
-  const response = await request.get("/test");
-  expect(response.status).toBe(200);
-  expect(response.body.message).toBe("pass!");
+describe("register works", () => {
+  it("doesn't allow register if passwords don't match", async () => {
+    const response = await request.post("/register").send({ username: "randomUserName", password: "password", passwordConfirmation: "password1" });
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({ error: "Invalid credentials" });
+  });
+
+  it("doesn't allow register if user exists", async () => {
+    const response = await request.post("/register").send({ username: "adminTest", password: "password", passwordConfirmation: "password" });
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({ error: "Invalid credentials" });
+  });
 });
