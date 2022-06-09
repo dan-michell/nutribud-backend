634d221e819f1d9d8fc0d22af9dbc5c34990f34c
Dan Michell <dan.michell1000@gmail.com>
2022-06-09 11:27:19 +0100

    Register handler tests complete

--- server.test.js
+++ server.test.js
@@ -2,16 +2,28 @@ const app = require("./server");
 const supertest = require("supertest");
 const request = supertest(app);
 
-describe("register works", () => {
+describe("register handler", () => {
   it("doesn't allow register if passwords don't match", async () => {
-    const response = await request.post("/register").send({ username: "randomUserName", password: "password", passwordConfirmation: "password1" });
+    const response = await request
+      .post("/register")
+      .send({ username: "randomUserName", password: "password", passwordConfirmation: "password1" });
     expect(response.statusCode).toEqual(400);
     expect(response.body).toEqual({ error: "Invalid credentials" });
   });
 
   it("doesn't allow register if user exists", async () => {
-    const response = await request.post("/register").send({ username: "adminTest", password: "password", passwordConfirmation: "password" });
+    const response = await request
+      .post("/register")
+      .send({ username: "adminTest", password: "password", passwordConfirmation: "password" });
     expect(response.statusCode).toEqual(400);
     expect(response.body).toEqual({ error: "Invalid credentials" });
+  });
+
+  it("registers user if valid inputs are entered", async () => {
+    const response = await request
+      .post("/register")
+      .send({ username: "adminTest2", password: "password", passwordConfirmation: "password" });
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({ response: "Successful registration" });
   });
 });
