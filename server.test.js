111824a04c71a636bd3812187307a099c79709a8
Dan Michell <dan.michell1000@gmail.com>
2022-06-09 15:10:27 +0100

    Added more tests

--- server.test.js
+++ server.test.js
@@ -36,6 +36,22 @@ describe("GET login handler", () => {
   });
 });
 
+describe("DELETE login handler", () => {
+  it("returns error if user is not logged in", async () => {
+    const response = await request.delete("/login");
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({ error: "User not logged in" });
+  });
+
+  it("returns error if user is not logged in", async () => {
+    const loginResponse = await request.post("/login").send({ username: "adminTest2", password: "password" });
+    const sessionId = loginResponse.headers["set-cookie"][0].split(";")[0].split("=")[1];
+    const response = await request.delete("/login").set("Cookie", `sessionId=${sessionId}`);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({ response: "Successfully logged out" });
+  });
+});
+
 describe("POST register handler", () => {
   it("doesn't allow register if passwords don't match", async () => {
     const response = await request
@@ -59,5 +75,24 @@ describe("POST register handler", () => {
       .send({ username: "adminTest3", password: "password", passwordConfirmation: "password" });
     expect(response.statusCode).toEqual(400);
     // expect(response.body).toEqual({ response: "Successful registration" });
+  });
+});
+
+describe("GET search-text handler", () => {
+  it("returns an error if search query not supplied", async () => {
+    const response = await request.get("/search-text").query({ item: "" });
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({ error: "Missing item" });
+  });
+
+  it("returns an error if the supplied text does not yield any results", async () => {
+    const response = await request.get("/search-text").query({ item: "xxxx" });
+    expect(response.body).toEqual({ error: `xxxx not found` });
+  });
+
+  it("returns formatted parsed data with valid search query", async () => {
+    const response = await request.get("/search-text").query({ item: "kaffir" });
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual();
   });
 });
